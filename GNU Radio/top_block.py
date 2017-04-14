4318dd8261322c979d7ef901e44c4ed9f03b1176
PrieureDeSion <dhruv.ilesh@gmail.com>
2017-04-15 00:01:38 +0530

    Custom Blocks Finally

--- GNU Radio/top_block.py
+++ GNU Radio/top_block.py
@@ -1,107 +1,128 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
 ##################################################
-# Gnuradio Python Flow Graph
+# GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Thu Apr 13 16:27:40 2017
+# Generated: Sat Apr 15 00:00:57 2017
 ##################################################
 
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
 from gnuradio import analog
-from gnuradio import audio
+from gnuradio import blocks
 from gnuradio import eng_notation
-from gnuradio import filter
 from gnuradio import gr
+from gnuradio import wxgui
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
+from gnuradio.wxgui import constsink_gl
 from gnuradio.wxgui import forms
 from grc_gnuradio import wxgui as grc_wxgui
 from optparse import OptionParser
+import epy_block_0
 import wx
+
 
 class top_block(grc_wxgui.top_block_gui):
 
     def __init__(self):
         grc_wxgui.top_block_gui.__init__(self, title="Top Block")
-        _icon_path = "/usr/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
-        self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
 
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 8000
-        self.f = f = 500
+        self.samp_rate = samp_rate = 32000
+        self.amplitude = amplitude = 0
 
         ##################################################
         # Blocks
         ##################################################
-        _f_sizer = wx.BoxSizer(wx.VERTICAL)
-        self._f_text_box = forms.text_box(
+        _amplitude_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._amplitude_text_box = forms.text_box(
         	parent=self.GetWin(),
-        	sizer=_f_sizer,
-        	value=self.f,
-        	callback=self.set_f,
-        	label="f",
+        	sizer=_amplitude_sizer,
+        	value=self.amplitude,
+        	callback=self.set_amplitude,
+        	label='amplitude',
         	converter=forms.float_converter(),
         	proportion=0,
         )
-        self._f_slider = forms.slider(
+        self._amplitude_slider = forms.slider(
         	parent=self.GetWin(),
-        	sizer=_f_sizer,
-        	value=self.f,
-        	callback=self.set_f,
-        	minimum=100,
-        	maximum=100000,
+        	sizer=_amplitude_sizer,
+        	value=self.amplitude,
+        	callback=self.set_amplitude,
+        	minimum=0,
+        	maximum=0.6,
         	num_steps=100,
         	style=wx.SL_HORIZONTAL,
         	cast=float,
         	proportion=1,
         )
-        self.Add(_f_sizer)
-        self.rational_resampler_xxx_0 = filter.rational_resampler_fff(
-                interpolation=48000,
-                decimation=8000,
-                taps=None,
-                fractional_bw=None,
+        self.Add(_amplitude_sizer)
+        self.wxgui_constellationsink2_0 = constsink_gl.const_sink_c(
+        	self.GetWin(),
+        	title='Constellation Plot',
+        	sample_rate=samp_rate,
+        	frame_rate=5,
+        	const_size=2048,
+        	M=4,
+        	theta=0,
+        	loop_bw=6.28/100.0,
+        	fmax=0.06,
+        	mu=0.5,
+        	gain_mu=0.005,
+        	symbol_rate=samp_rate/4.,
+        	omega_limit=0.005,
         )
-        self.audio_sink_0 = audio.sink(48000, "", True)
-        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, f, 0.5, 0)
+        self.Add(self.wxgui_constellationsink2_0.win)
+        self.epy_block_0 = epy_block_0.blk(power_param=1.0)
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
+        self.blocks_add_xx_0 = blocks.add_vcc(1)
+        self.analog_random_uniform_source_x_0 = analog.random_uniform_source_i(0, 4, 0)
+        self.analog_noise_source_x_0 = analog.noise_source_c(analog.GR_GAUSSIAN, amplitude, 42)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_sig_source_x_0, 0), (self.rational_resampler_xxx_0, 0))
-        self.connect((self.rational_resampler_xxx_0, 0), (self.audio_sink_0, 0))
-
-
-# QT sink close method reimplementation
+        self.connect((self.analog_noise_source_x_0, 0), (self.blocks_add_xx_0, 1))
+        self.connect((self.analog_random_uniform_source_x_0, 0), (self.epy_block_0, 0))
+        self.connect((self.blocks_add_xx_0, 0), (self.blocks_throttle_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.wxgui_constellationsink2_0, 0))
+        self.connect((self.epy_block_0, 0), (self.blocks_add_xx_0, 0))
 
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+        self.wxgui_constellationsink2_0.set_sample_rate(self.samp_rate)
+        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
 
-    def get_f(self):
-        return self.f
+    def get_amplitude(self):
+        return self.amplitude
 
-    def set_f(self, f):
-        self.f = f
-        self.analog_sig_source_x_0.set_frequency(self.f)
-        self._f_slider.set_value(self.f)
-        self._f_text_box.set_value(self.f)
+    def set_amplitude(self, amplitude):
+        self.amplitude = amplitude
+        self._amplitude_slider.set_value(self.amplitude)
+        self._amplitude_text_box.set_value(self.amplitude)
+        self.analog_noise_source_x_0.set_amplitude(self.amplitude)
 
-if __name__ == '__main__':
-    import ctypes
-    import os
-    if os.name == 'posix':
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print "Warning: failed to XInitThreads()"
-    parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
-    (options, args) = parser.parse_args()
-    tb = top_block()
+
+def main(top_block_cls=top_block, options=None):
+
+    tb = top_block_cls()
     tb.Start(True)
     tb.Wait()
 
+
+if __name__ == '__main__':
+    main()
