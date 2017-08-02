51643d9e0b4b634d23297f380365ca0f238012d8
PrieureDeSion <dhruv.ilesh@gmail.com>
2017-08-02 22:15:28 +0530

    Wait etc.

--- Operating Systems/system_calls.c
+++ Operating Systems/system_calls.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
@@ -8,5 +10,23 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 	pid_t pid[12];
+	int i, status;
+	for (i = 0; i <= 10; i++) {
+		if ((pid[i] = fork()) == 0) {
+			// Child Process executes
+			printf("Child %d is born with PID %d\n", i, getpid());
+			sleep(((11 - i) % 5));
+			exit(20 + i);
+		}
+	}
+
+	for (int i = 0; i <= 10; i++) {
+		pid_t pidi = wait(&status);
+		if (WIFEXITED(status)) {
+			printf("Child %d exited with status %d\n", pidi, WEXITSTATUS(status));
+		}
+	}
+
+
 	return 0;
 }
